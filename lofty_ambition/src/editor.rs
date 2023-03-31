20ec31e7c89af87e88ea1a56df1083b75319f65f
vker <30568622+lucklyisgood@users.noreply.github.com>
2023-03-31 21:10:45 +0800

    add draw rect & context menu logic

--- lofty_ambition/src/editor.rs
+++ lofty_ambition/src/editor.rs
@@ -1,4 +1,3 @@
-use eframe::epaint::Stroke;
 use eframe::{egui, epaint};
 
 use crate::graph_ui::GraphUI;
@@ -7,6 +6,9 @@ use crate::menu_ui::ContextMenuUI;
 pub struct Editor {
     pub graph_ui: GraphUI<u32>,
     pub context_menu_ui: ContextMenuUI,
+    pub box_select_start_pos: Option<egui::Pos2>,
+    pub pan: egui::Vec2,
+    pub zoom: f32,
 }
 
 impl Editor {
@@ -14,6 +16,10 @@ impl Editor {
         Self {
             graph_ui: GraphUI::new(la_node_graph::Graph::new()),
             context_menu_ui: ContextMenuUI::default(),
+            box_select_start_pos: None,
+
+            pan: egui::Vec2::ZERO,
+            zoom: 1.0,
         }
     }
 
@@ -27,15 +33,97 @@ impl Editor {
         //             self.context_menu_ui.draw(ui);
         //         });
         // });
-        let plot_size = egui::Vec2::new(800.0, 600.0);
-        let plot_background = epaint::RectShape {
-            rect: egui::Rect::from_min_size(egui::Pos2::ZERO, plot_size),
-            rounding: epaint::Rounding::same(0.0),
-            fill: egui::Color32::from_rgba_premultiplied(255, 255, 0, 200).into(),
-            stroke: Stroke::NONE,
-        };
-        ui.painter().add(plot_background);
+        
+        let view_rect = ui.max_rect();
+        // ignore hover event in rect;
+        ui.allocate_rect(view_rect, egui::Sense::hover());
+
+        let pointer_pos = ui.input(|i|i.pointer.hover_pos().unwrap_or(egui::Pos2::ZERO));
+        let pointer_in_view = view_rect.contains(pointer_pos);
+        let mut drag_start_in_bg = false;
+        let mut drag_end_in_bg = false;
+        let mut click_in_bg = false;
+
+        // Draw nodes
+        self.graph_ui.draw(ui, &self.pan, self.zoom);
+
+        let r = ui.allocate_rect(ui.min_rect(), egui::Sense::click().union(egui::Sense::drag()));
+        if r.clicked() {
+            click_in_bg = true;
+        } else if r.drag_started() {
+            drag_start_in_bg = true;
+        } else if r.drag_released() {
+            drag_end_in_bg = true;
+        }
+
+
+        // handle box select
+        if let Some(select_start_pos) = self.box_select_start_pos {
+            let select_rect = egui::Rect::from_two_pos(select_start_pos, pointer_pos);
+            let fill_color = egui::Color32::from_rgba_premultiplied(200, 200, 200, 20);
+            let stroke_color = egui::Color32::from_rgba_unmultiplied(200, 200, 200, 180);
+            ui.painter().rect(select_rect, 2.0, fill_color, epaint::Stroke::new(3.0, stroke_color));
+
+            // self.selected_nodes = node_rects.into_iter().filter_map(|(node_id, rect)| { if selection_rect.intersects(rect) { Some(node_id) } else { None } }).collect();
+        }
+
+        let is_pirmary_down = ui.input(|i| i.pointer.primary_down());
+        let is_pirmary_release = ui.input(|i| i.pointer.primary_released());
+
+        if drag_start_in_bg && is_pirmary_down {
+            self.box_select_start_pos = Some(pointer_pos);
+        }
+        if drag_end_in_bg || is_pirmary_release {
+            self.box_select_start_pos = None;
+        }
+
+        // show context menu
+        self.show_context_menu(ui, r, &pointer_pos, &view_rect);
+        
     }
+
+    fn show_context_menu(&mut self, ui: &mut egui::Ui, r: egui::Response, pointer_pos: &egui::Pos2, view_rect: &egui::Rect) -> egui::Response {
+        r.context_menu(|ui| {
+            ui.menu_button("readbook", |ui| {
+                if ui.button("spine").clicked() {
+                    tracing::info!("create node readbook/spine");
+                    let pos = egui::pos2(pointer_pos.x, pointer_pos.y) - self.pan - view_rect.min.to_vec2();
+                    self.graph_ui.add_node(10086, pos);
+                    ui.close_menu();
+                }
+            });
+
+            ui.menu_button("msic", |ui| {
+                if ui.button("spine").clicked() {
+                    tracing::info!("create node msic/spine");
+                    ui.close_menu();
+                }
+            });
+
+            if ui.button("test").clicked() {
+                tracing::info!("create node test");
+                ui.close_menu();
+            }
+        })
+    }
+}
+
+#[allow(dead_code)]
+fn draw_connection(painter: &egui::Painter, src_pos: egui::Pos2, dst_pos: egui::Pos2, color: egui::Color32) {
+    let connection_stroke = egui::Stroke { width: 5.0, color };
+
+    let control_scale = ((dst_pos.x - src_pos.x) / 2.0).max(30.0);
+    let src_control = src_pos + egui::Vec2::X * control_scale;
+    let dst_control = dst_pos - egui::Vec2::X * control_scale;
+
+    let bezier = epaint::CubicBezierShape::from_points_stroke(
+        [src_pos, src_control, dst_control, dst_pos],
+        false,
+        egui::Color32::TRANSPARENT,
+        connection_stroke,
+    );
+
+    painter.add(bezier);
 }
 
 
--- lofty_ambition/src/graph_ui.rs
+++ lofty_ambition/src/graph_ui.rs
@@ -1,16 +1,43 @@
-use la_node_graph::Graph;
+use la_node_graph::{ Graph, NodeId};
+use eframe::{egui, epaint};
 
 pub struct GraphUI<CustomDataType> {
     pub graph: Graph<CustomDataType>,
+    pub node_ui_data_list: slotmap::SecondaryMap<NodeId, egui::Pos2>,
+    pub node_order_list: Vec<NodeId>,
 }
 
 impl<CustomDataType> GraphUI<CustomDataType> {
     pub fn new(graph: Graph<CustomDataType>) -> Self {
-        Self { graph }
+        Self {
+            graph,
+            node_ui_data_list: slotmap::SecondaryMap::default(),
+            node_order_list: Vec::default(),
+        }
+    }
+
+    fn draw_node(&self, ui: &mut egui::Ui, pos: &egui::Pos2) {
+        let base_shape = epaint::RectShape {
+            rect: egui::Rect::from_min_size(egui::pos2(pos.x, pos.y), egui::vec2(200.0, 200.0)),
+            rounding: epaint::Rounding::same(3.0),
+            fill: egui::Color32::from_rgb(60, 60, 60),
+            stroke: epaint::Stroke { width: 3.0, color:  egui::Color32::from_rgb(255, 0, 0) },
+        };
+        ui.painter().add(base_shape);
     }
 
     #[allow(dead_code)]
-    pub fn draw(&mut self, _ui: &mut eframe::egui::Ui) {
+    pub fn draw(&mut self, ui: &mut egui::Ui, _pan: &egui::Vec2, _zoom: f32) {
+        for id in self.node_order_list.iter().copied() {
+            if let Some(item_pos) = self.node_ui_data_list.get(id) {
+                self.draw_node(ui, item_pos);
+            }
+        }
+    }
 
+    pub fn add_node(&mut self, custom_data: CustomDataType, pos: egui::Pos2) {
+        let id = self.graph.add_node("Node".into(), custom_data, |_graph, _id| {});
+        self.node_ui_data_list.insert(id, pos);
+        self.node_order_list.push(id);
     }
 }
